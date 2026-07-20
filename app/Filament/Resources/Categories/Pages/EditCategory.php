50c2539043217b1391b921356f838de48c21bac4
nisa6delgado <nisadelgado@gmail.com>
2026-07-20 17:07:37 -0400

    Íconos en botones de acción

--- app/Filament/Resources/Categories/Pages/EditCategory.php
+++ app/Filament/Resources/Categories/Pages/EditCategory.php
@@ -13,7 +13,8 @@ class EditCategory extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            DeleteAction::make(),
+            DeleteAction::make()
+                ->icon('heroicon-o-trash'),
         ];
     }
 }
